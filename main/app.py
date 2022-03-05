797c42813cbae2b36c782308a9cf3c51ffae2932
MoonDunes <mitgerard@gmail.com>
2022-03-05 16:15:52 -0500

    Added ability to play a card

--- main/app.py
+++ main/app.py
@@ -1,11 +1,18 @@
+import flask
 from flask import Flask
-
 
 def create_app():
     app = Flask(__name__)
+    played_ = {'lastPlayed': '0'}
 
     @app.route('/')
     def getGameState():
-        return {'Hello': 'World'}
+        return played_
+
+    @app.route('/', methods=['PUT'])
+    def putGameState():
+        nonlocal played_
+        played_ = flask.request.json
+        return played_
 
     return app
--- test/test_sample.py
+++ test/test_sample.py
@@ -1,4 +1,8 @@
-def test_example(client):
+def test_play_a_card(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == {'Hello': 'World'}
+    assert response.json == {'lastPlayed': '0'}
+    response = client.put('/', json={'lastPlayed': '1'})
+    assert response.json == {'lastPlayed': '1'}
+    response = client.get('/')
+    assert response.json == {'lastPlayed': '1'}
